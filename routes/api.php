f776fa7e19d280cfa11b3af41ae6d35138f7ac94
Glen Ochieng <glenochieng045@gmail.com>
2025-04-23 21:45:56 +0300

    feat: migrate from built-in Geocoding to explicitly using the Geocoding API

--- routes/api.php
+++ routes/api.php
@@ -7,18 +7,41 @@ Route::get('/user', function (Request $request) {
     return "Hello user";
 });
 
-Route::get('/weather', function () {
+Route::get('/weather/{city}', function (string $city) {
     $apiKey = env('OPENWEATHERMAP_API_KEY');
-    $city = 'Nairobi';
-    $response = Http::get("https://api.openweathermap.org/data/2.5/weather", [
+
+    // Get co-ordinates from the Geocoding API
+    $geocodeResponse = Http::get("https://api.openweathermap.org/data/2.5/weather", [
         'q' => $city,
         'appid' => $apiKey,
+        'units' => "metric"
     ]);
 
-    if ($response->successful()) {
-        return $response->json();
+    if ($geocodeResponse->successful()) {
+        $coordinates = $geocodeResponse->json();
+        $lat = $coordinates['coord']['lat'];
+        $lon = $coordinates['coord']['lon'];
+    } else {
+        return response()->json(['error' => 'Unable to fetch coordinates'], $geocodeResponse->status());
     }
 
-    return response()->json(['error' => 'Unable to fetch weather data'], $response->status());
+    // Use the longitude and latitude to get the weather data
+    $weatherDataResponse = Http::get("https://api.openweathermap.org/data/2.5/weather", [
+        'lat' => $lat,
+        'lon' => $lon,
+        'appid' => $apiKey,
+        'units' => 'metric'
+    ]);
+
+    // Check if the response is successful
+    if ($weatherDataResponse->successful()) {
+        $weatherData = $weatherDataResponse->json();
+        $weatherData["weather"] = $weatherData["weather"][0];
+    } else {
+        return response()->json(['error' => 'Unable to fetch weather data'], $weatherDataResponse->status());
+    }
+    return response()->json([
+        'current' => $weatherData,
+    ]);
 });
 
